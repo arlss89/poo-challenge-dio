a466d9812946c3fae703c12242a58a5d749e64c7
Alyson Reis <arlss89@gmail.com>
2023-12-23 12:01:08 -0300

    [models] Implementing a super class named as Smartphone and other sub classes, named Iphone and Nokia.

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,29 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string imei, int memoria, string cameraQuality) : base(numero, modelo, imei, memoria) 
+        {
+
+            if (string.IsNullOrWhiteSpace(cameraQuality))
+            {
+                throw new ArgumentException("A qualidade da câmera não pode ser nula ou vazia.");
+            }
+            
+            CameraQuality = cameraQuality;
+        }
+
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+
+        public string CameraQuality { get; private set; }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            _logger.WriteLine($"Installing app for Iphone: {nomeApp}");
+        }
+
     }
 }
--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,24 +1,51 @@
+using pooChallengeDioMain.Interfaces;
+
 namespace DesafioPOO.Models
 {
-    public abstract class Smartphone
+    public abstract class Smartphone : BaseEntity
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo {get;private set;}
+        public string Imei{get; private set;}
+        public int Memoria {get;private set;}
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
+            if (string.IsNullOrWhiteSpace(numero))
+            {
+                throw new ArgumentException("Número do smartphone não pode ser nulo ou vazio.");
+            }
+
+            if (string.IsNullOrWhiteSpace(modelo))
+            {
+                throw new ArgumentException("Modelo do smartphone não pode ser nulo ou vazio.");
+            }
+
+            if (string.IsNullOrWhiteSpace(imei))
+            {
+                throw new ArgumentException("IMEI do smartphone não pode ser nulo ou vazio.");
+            }
+
+            if (memoria <= 0)
+            {
+                throw new ArgumentException("A memória do smartphone deve ser maior que zero.");
+            }
+
+
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+           _logger.WriteLine(this.getClassName() + ": " + "Ligando...");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            _logger.WriteLine(this.getClassName() + ": " + "Recebendo ligação...");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
